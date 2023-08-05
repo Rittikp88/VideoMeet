331c48c320230081a00450c571213940b0a00957
Rittik Prasad <prasadrittik073@gmail.com>
2023-08-06 02:02:32 +0530

    connect to server

--- webView-Video-call/src/context/SocketProvider.jsx
+++ webView-Video-call/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 ;}
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io.connect("localhost:8080"),[]);
+    const socket = useMemo(() => io.connect("https://vercel.com/rittikp88/video-meet-server/9KLC1EsPPiqQ46ViSBZ56pdjBebG/api"),[]);
     return (
     <SocketContext.Provider value ={socket}>
         {props.children}
