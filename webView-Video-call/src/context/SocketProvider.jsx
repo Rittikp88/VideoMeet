85ddf754fe775b3d77a5661bd838af5a972f1962
Rittik Prasad <prasadrittik073@gmail.com>
2023-08-06 20:59:08 +0530

    io connected

--- webView-Video-call/src/context/SocketProvider.jsx
+++ webView-Video-call/src/context/SocketProvider.jsx
@@ -1,45 +1,18 @@
-// import React, {createContext, useMemo, useContext} from "react";
-// import { io } from "socket.io-client";
-
-// const SocketContext = createContext(null);
-
-// export const useSocket = () => {
-//     const socket = useContext(SocketContext);
-//     return socket
-// ;}
-
-// export const SocketProvider = (props) => {
-//     const socket = useMemo(() => io.connect("https://video-meet-server-86uoc4juh-rittikp88.vercel.app/"),[{ secure: true, transports: [ "flashsocket","polling","websocket" ] }] );
-//     return (
-//     <SocketContext.Provider value ={socket}>
-//         {props.children}
-//     </SocketContext.Provider>
-//     )
-// }
-
-// ClientSide.js (Updated)
-
-import React, { createContext, useMemo, useContext } from "react";
+import React, {createContext, useMemo, useContext} from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-  const socket = useContext(SocketContext);
-  return socket;
-};
+    const socket = useContext(SocketContext);
+    return socket
+;}
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() =>
-    io("https://video-meet-server-86uoc4juh-rittikp88.vercel.app", {
-      secure: true,
-      transports: ["websocket", "polling", "flashsocket"],
-    })
-  , []); // Pass an empty dependency array to useMemo to create the socket connection only once
-
-  return (
-    <SocketContext.Provider value={socket}>
-      {props.children}
+    const socket = useMemo(() => io.connect("http://localhost:8080/"),[]);
+    return (
+    <SocketContext.Provider value ={socket}>
+        {props.children}
     </SocketContext.Provider>
-  );
-};
+    )
+}
