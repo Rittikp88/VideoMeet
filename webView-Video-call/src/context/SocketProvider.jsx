0d315f96be0ac886e185811ae630310a0cbbf66a
Rittik Prasad <prasadrittik073@gmail.com>
2023-08-08 23:33:02 +0530

    https changes

--- webView-Video-call/src/context/SocketProvider.jsx
+++ webView-Video-call/src/context/SocketProvider.jsx
@@ -12,7 +12,7 @@ export const SocketProvider = (props) => {
     const host = '44.207.3.207';
     const port = 8080;
     // const socket = useMemo(() => io.connect("https://d670-2405-201-4018-93be-e021-854f-8df0-3410.ngrok-free.app"),[]);
-    const socket = useMemo(() => io.connect(`http://${host}:${port}`));
+    const socket = useMemo(() => io.connect(`https://${host}:${port}`));
     return (
     <SocketContext.Provider value ={socket}>
         {props.children}
